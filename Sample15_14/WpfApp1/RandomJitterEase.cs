8eb993e3f50cd4063f9809ae4785b7e4a11eba5f
NK <fipsiksi@yandex.ru>
2018-05-11 11:45:33 +0300

    init

---
+++ Sample15_14/WpfApp1/RandomJitterEase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace WpfApp1
+{
+    public class RandomJitterEase : EasingFunctionBase
+    {
+        // Генератор случайных чисел
+        private Random rand = new Random();
+
+        protected override double EaseInCore(double normalizedTime)
+        {
+            if (normalizedTime == 1) return 1;
+            return Math.Abs(normalizedTime - (double)rand.Next(0, 10) / (2010 - Jitter));
+        }
+
+        public int Jitter
+        {
+            get { return (int)GetValue(JitterProperty); }
+            set { SetValue(JitterProperty, value); }
+        }
+
+        public static readonly DependencyProperty JitterProperty =
+            DependencyProperty.Register("Jitter", typeof(int), typeof(RandomJitterEase),
+            new UIPropertyMetadata(1000), new ValidateValueCallback(ValidateJitter));
+
+        private static bool ValidateJitter(object value)
+        {
+            int jitterValue = (int)value;
+            return ((jitterValue <= 2000) && (jitterValue >= 0));
+        }
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new RandomJitterEase();
+        }
+    }
+}
